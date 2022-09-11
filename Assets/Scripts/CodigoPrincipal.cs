47cb63a343b09b4d7b5710c0c3d54e8f2a73e3ce
RuanPS01 <ruanpatrick.s@hotmail.com>
2022-09-10 22:36:37 -0300

    Implementação de código principal de captura do clique

--- Assets/Scripts/CodigoPrincipal.cs
+++ Assets/Scripts/CodigoPrincipal.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class CodigoPrincipal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float pontuacaoCookie = 0;
+    float pontuacaoCaixa = 0;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (pontuacaoCookie > 0) {
+            StartCoroutine(diminuiPontuacaoCookie());
+        }
+        if (pontuacaoCaixa > 0)
+        {
+            StartCoroutine(apagaPontuacaoCaixa());
+        }
+
+        if (ObjetoQueFoiTocado() == "Cookie_Imagem") 
+        {
+            Debug.Log("O Cookie foi tocado!");
+        }
     }
+
+    IEnumerator diminuiPontuacaoCookie()
+    {
+        while (pontuacaoCookie >= 0) {
+            yield return new WaitForSeconds(1);
+            pontuacaoCookie = pontuacaoCookie - 0.001f;
+        }
+    }
+
+    IEnumerator apagaPontuacaoCaixa()
+    {
+        while (pontuacaoCaixa > 0)
+        {
+            yield return new WaitForSeconds(5);
+            pontuacaoCaixa = 0;
+        }
+    }
+
+    string ObjetoQueFoiTocado() {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            RaycastHit2D hit = Physics2D.Raycast(touch.position, Vector2.zero);
+            if (hit)
+            {
+                return hit.transform.gameObject.name;
+            }
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, Vector2.zero);
+            if (hit)
+            {
+                return hit.transform.gameObject.name;
+            }
+        }
+            return "";
+    } 
+
 }
